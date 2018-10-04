4f8d0c6d8708bf669f4c9dbdb4f0ed66d20b9214
Cody Nivens <cody.nivens@gmail.com>
2018-10-04 09:13:58 -0700

    Add gate to update jenkins war file and plugins

--- jenkins/build_jenkins.sh
+++ jenkins/build_jenkins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#docker build --no-cache -t 127.0.0.1:30400/jenkins:latest -f Dockerfile ./ && docker push 127.0.0.1:30400/jenkins:latest
 docker build -t 127.0.0.1:30400/jenkins:latest -f Dockerfile ./ && docker push 127.0.0.1:30400/jenkins:latest
